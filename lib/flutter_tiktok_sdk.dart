825b50b414f6a800f73964e5a8f2294cebb43b16
Kota Hayashi <geukfyger@gmail.com>
2022-03-22 20:56:59 +0900

    Add: dartdoc

--- lib/flutter_tiktok_sdk.dart
+++ lib/flutter_tiktok_sdk.dart
@@ -5,4 +5,4 @@ import 'package:flutter/services.dart';
 
 part 'src/tiktok_permission_type.dart';
 part 'src/tiktok_sdk.dart';
-part 'src/tiktok_auth_result.dart';
+part 'src/tiktok_login_result.dart';
