906a6e6b6190257461862fbc9e8d8819b1851a55
K9i-0 <geukfyger@gmail.com>
2022-06-03 07:19:29 +0900

    🔥removed old scope

--- lib/src/tiktok_permission_type.dart
+++ lib/src/tiktok_permission_type.dart
@@ -2,7 +2,6 @@ part of '../flutter_tiktok_sdk.dart';
 
 enum TikTokPermissionType {
   userInfoBasic('user.info.basic'),
-  shareSoundCreate('share.sound.create'),
   videoList('video.list'),
   videoUpload('video.upload');
 
@@ -15,8 +14,6 @@ TikTokPermissionType? _fromScopeName(String scopeName) {
   switch (scopeName) {
     case 'user.info.basic':
       return TikTokPermissionType.userInfoBasic;
-    case 'share.sound.create':
-      return TikTokPermissionType.shareSoundCreate;
     case 'video.list':
       return TikTokPermissionType.videoList;
     case 'video.upload':
